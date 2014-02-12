a326777897a578bf7ccf9b37ff4d211ed2badaad
Tpt <thomaspt@hotmail.fr>
2014-02-12 12:02:57 +0100

    Details in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,18 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    if(argc==1)
-    {
-        cerr<<"resol: fatal error: no input files\nresolution terminated."<<endl<<endl;
-        exit(EXIT_FAILURE);
+int main(int argc, char *argv[]) {
+    if(argc == 1) {
+        cerr << "resol: fatal error: no input files\nresolution terminated." << endl << endl;
+       return EXIT_FAILURE;
     }
 
-    CFormule* formule=new CFormule(argv[1]);
+    CFormule formule( argv[1] );
     clock_t t;
-    t=clock();
-    formule->solve();
-    t=clock()-t;
-    printf("c Resolu en : %f secondes\n",static_cast<double>(t)/static_cast<double>(1000));
-    return 0;
+    t = clock();
+    formule.solve();
+    t = clock() - t;
+    printf("c Resolu en : %f secondes\n", static_cast<double>(t) / static_cast<double>(CLOCKS_PER_SEC));
+
+    return EXIT_SUCCESS;
 }
