73787f13e0a97008c7712a6066ff139f30633b5c
Tpt <thomaspt@hotmail.fr>
2014-02-23 20:37:15 +0100

    Initialisation et structure du backtraking
    Il y a une petite duplication du code mais cela àmha est moins gênant que la rigidité lié à l'héritage. Mais si il y a d'autre duplication, ce sera sans doute opportun de créer un AbstractDPLLSolveur dont hériteront les deux autres (ce sera le cas si on sort des trucs de Formule pour avoir un contrôle plus fin sur la simplification)

--- include/DPLLSurveilleSolveur.h
+++ include/DPLLSurveilleSolveur.h
@@ -10,7 +10,11 @@ public:
     DPLLSurveilleSolveur(Formule &formule);
     bool isSatifiable();
 private:
-    std::unordered_map<int,std::pair<int,int>> watchedLiterals;
+    std::unordered_map<int,std::pair<int,int>> literauxSurveilles;
+    bool assigneUneVariableEtRetourneSatisfiabilite();
+    bool assigneVariableEtRetourneSatisfiabilite(int varId, bool val);
+    void initialiserLiterauxSurveilles();
+    int trouveLiteralASurveille(Clause* clause, int autreLiteral = 0); /// Mettre autreLiteral à 0 quand il n'y a pas encore de literal surveillé. Retourne autreLiteral si, et seulement si, aucun autre litéral convenable n'est trouvé
 };
 
 #endif
--- src/DPLLSurveilleSolveur.cpp
+++ src/DPLLSurveilleSolveur.cpp
@@ -2,10 +2,60 @@
 #include<unordered_set>
 #include "../include/DPLLSurveilleSolveur.h"
 
+using namespace std;
+
 DPLLSurveilleSolveur::DPLLSurveilleSolveur(Formule &formule_) : Solveur(formule_)
 {}
 
 bool DPLLSurveilleSolveur::isSatifiable()
 {
-    return true; //TODO
+    return assigneUneVariableEtRetourneSatisfiabilite();
+}
+
+bool DPLLSurveilleSolveur::assigneUneVariableEtRetourneSatisfiabilite()
+{
+    int varId = 1; //TODO
+    if(varId == -1)
+        return true;
+    
+    Formule save = formule;
+    
+    if(assigneVariableEtRetourneSatisfiabilite(varId, true))
+        return true;
+    
+    //backtrack
+    formule = save;
+    if(assigneVariableEtRetourneSatisfiabilite(varId, false))
+        return true;
+    
+    return false;
+}
+
+bool DPLLSurveilleSolveur::assigneVariableEtRetourneSatisfiabilite(int varId, bool val)
+{
+    Variable* var = formule.getVar(varId - 1);
+    var->setVal(val);
+    std::cout << "assigne " << var->getId() << " a " << var->getVal() << std::endl;
+    
+    //TODO simplification
+    
+    return assigneUneVariableEtRetourneSatisfiabilite();
+}
+
+void DPLLSurveilleSolveur::initialiserLiterauxSurveilles()
+{
+    for(Clause* clause : formule.getClauses())
+    {
+        int premierLiteral = trouveLiteralASurveille(clause);
+        int secondLiteral = trouveLiteralASurveille(clause, premierLiteral);
+        literauxSurveilles[clause->getUid()] = pair<int,int>(premierLiteral, secondLiteral);
+    }
+}
+
+int DPLLSurveilleSolveur::trouveLiteralASurveille(Clause* clause, int autreLiteral)
+{
+    for(Literal* literal : clause->getLiteraux())
+        if((!literal->isAssignee() || literal->getVar()) && literal->getId() != autreLiteral)
+            return literal->getId();
+    return autreLiteral;
 }
