6b2e8ba7d050f5f5fec8bb8e253dea3315d44abb
Sir Isaac Newton <chevalier_m@orange.fr>
2014-03-30 15:26:35 +0200

    Générateur de graphe aléatoires.

---
+++ gen/genGraphe.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<fstream>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include<sstream>
+#include<vector>
+#include<unordered_set>
+
+using namespace std;
+
+/*string to_string(int number)
+{
+   stringstream ss;
+   ss << number;
+   return ss.str();
+}*/
+
+pair<int, int> next(int n, pair<int, int> e);
+
+pair<int, int> next(int n, pair<int, int> e)
+{
+    if(e.second<n-1)
+    {
+        e.second++;
+        return e;
+    }
+    else if(e.first<n-2)
+    {
+        e.first++;
+        e.second=e.first+1;
+        return e;
+    }
+    e.first=1;
+    e.second=2;
+    return e;
+}
+
+int main(int argc, char* argv[])
+{
+    int n;
+    int m;
+    
+    if(argc<3)
+    {
+        cout<<"Nombre de sommets : ";
+        cin>>n;
+        cout<<"Nombre d'arêtes': ";
+        cin>>m;
+    }
+    else
+    {
+        n=atoi(argv[1]);
+        m=atoi(argv[2]);
+    }
+    
+    if(n*(n-1)<2*m)
+    {
+        cerr<<"Trop d'arêtes ! "<<endl;
+        return EXIT_FAILURE;
+    }
+
+    srand(static_cast<unsigned int>(time(NULL)));
+    string filename;
+
+    if(argc<4)
+    {
+        filename="graphe_";
+        filename+= to_string(n);
+        filename+="_";
+        filename+= to_string(m);
+        filename+=".txt";
+    }
+    else
+    {
+        filename=argv[3];
+    }
+
+
+    ofstream file(filename);
+
+
+    vector<unordered_set<int>> adj(n);
+    pair<int, int> e;
+    
+    for(int i=0;i<m;++i)
+    {
+        e.first=rand()%(n-1);
+        e.second=(rand()%(n-1-e.first))+e.first+1;
+        while(adj[e.first].find(e.second)!=adj[e.first].end())
+        {
+            e=next(n,e);
+        }
+        adj[e.first].insert(e.second);
+    }
+    
+    file<<"c Créé aléatoirement par notre programme de génération de graphe"<<endl;
+    file<<"p edge"<<n<<" "<<m<<endl;    
+
+    for(int i=0;i<n;++i)
+    {
+        for(int w : adj[i])
+        {
+            file<<"e "<<i<<" "<<w<<endl;
+        }
+    }
+    
+
+    file.close();
+
+    return 0;
+}
